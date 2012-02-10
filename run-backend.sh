eba2bf7254212d71da70dbfc0f964179656ccb33
Kyle Milz <kmilz@ucalgary.ca>
2012-02-10 16:03:43 -0700

    Commit a sample php backend.
    Currently the backend isn't working properly, it needs at least a
    working db connection.
    
    This is a reference implementation of the gpgAuth backend however. It is
    not that sophisticated and improving this reference implementation would
    be a great starting point. More info at http://www.gpgauth.org/
    
    All other backends will be based off of this one. Note the README has
    been updated with more information and dependencies.
    
    No developer configuration should be necessary, I have shipped all of
    the config files in this repository.

--- run-backend.sh
+++ run-backend.sh
@@ -2,7 +2,7 @@
 
 if [ $# -ne 1 ]; then
 	echo "Must specify one backend to run, one of"
-	echo "  ${0} <--perl|--ruby>"
+	echo "  ${0} <--perl|--ruby|--php>"
 	exit
 fi
 
@@ -13,6 +13,15 @@ if [ "$1" == "--perl" ]; then
 	echo -e " -> the server can be accessed via 127.0.0.1:8080 in a web browser\n"
 	lighttpd -f conf/perl/lighttpd.conf -D
 fi
+
+if [ "$1" == "--php" ]; then
+	echo -ne "\e[1;35m"
+	echo "launching lighttpd php backend"
+	echo -ne "\e[0m"
+	echo -e " -> the server can be accessed via 127.0.0.1:8080 in a web browser\n"
+	lighttpd -f conf/php/lighttpd.conf -D
+fi
+
 
 if [ "$1" == "--ruby" ]; then
 	echo -ne "\e[1;31m"
