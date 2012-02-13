11f4eda6e17f1fb7897e2aa9cc319072065a71f0
Jeff Nicholson <jeff.nicholson99@gmail.com>
2012-02-13 14:43:10 -0700

    modified shell script to handle invalid args

--- run-backend.sh
+++ run-backend.sh
@@ -12,22 +12,21 @@ if [ "$1" == "--perl" ]; then
 	echo -ne "\e[0m"
 	echo -e " -> the server can be accessed via 127.0.0.1:8080 in a web browser\n"
 	lighttpd -f conf/perl/lighttpd.conf -D
-fi
-
-if [ "$1" == "--php" ]; then
+elif [ "$1" == "--php" ]; then
 	echo -ne "\e[1;35m"
 	echo "launching lighttpd php backend"
 	echo -ne "\e[0m"
 	echo -e " -> the server can be accessed via 127.0.0.1:8080 in a web browser\n"
 	lighttpd -f conf/php/lighttpd.conf -D
-fi
-
-
-if [ "$1" == "--ruby" ]; then
+elif [ "$1" == "--ruby" ]; then
 	echo -ne "\e[1;31m"
 	echo "launching ruby on rails backend"
 	echo -ne "\e[0m"
 	pushd ruby
 	rails server
 	popd
+else
+	echo "Must specify one backend to run, one of"
+	echo "  ${0} <--perl|--ruby|--php>"
+	exit
 fi
