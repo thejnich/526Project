2cadddaec333bbd3cffc10de7119432aa0cb515b
Masud Khan <masudio@gmail.com>
2012-02-15 01:29:41 -0700

    (ruby changes)finally got key server get request to work.  Home pages shows my public key.

--- ruby/app/controllers/home_controller.rb
+++ ruby/app/controllers/home_controller.rb
@@ -1,6 +1,19 @@
+require 'net/http'
+
 class HomeController < ApplicationController
 
   def home
     @text = "Welcome home, secure users.  Welcome home."
+    url = URI.parse('http://keyserver.ubuntu.com:11371/pks/lookup')
+    http = Net::HTTP.new(url.host, url.port)
+    params = { 'op' => 'get', 'search' => 'makha@ucalgary.ca' }
+    req = Net::HTTP::Get.new(url.path)
+    req.set_form_data(params)
+    req = Net::HTTP::Get.new(url.path + '?' + req.body)
+
+    res = http.request(req)
+    @text = @text
+
+    @key_response = res.body
   end
 end
