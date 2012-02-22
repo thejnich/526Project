eb3f538587473001009a4584fd0edf37dc7ae5b0
Kyle Milz <kmilz@ucalgary.ca>
2012-02-22 14:39:46 -0700

    perl: implement some of stage1 authentication.
    There are new dependencies on secure random number generators.
    The numbers generated are used for the challenge response so we don't
    want to use the vanilla rand() function.

--- perl/index.pl
+++ perl/index.pl
@@ -11,6 +11,10 @@ use HTTP::Headers;
 require Crypt::OpenPGP;
 require Crypt::OpenPGP::KeyRing;
 
+use Math::Random::Secure qw(irand);
+
+use Digest::MD5 qw(md5 md5_hex md5_base64);
+
 use URI::Escape;
 
 
@@ -48,12 +52,12 @@ while (my $q = new CGI::Fast) {
 	#warn($headerDump);
 
 	my $gpg_headers = HTTP::Headers->new;
-	$gpg_headers->header(-X_GPGAuth_Version => '1.3.0');
-	$gpg_headers->header(-X_GPGAuth_Requested => 'false');
-	$gpg_headers->header(-X_GPGAuth_Verify_URL => '/index.pl?server_verify');
-	$gpg_headers->header(-X_GPGAuth_Login_URL => '/index.pl?login');
-	$gpg_headers->header(-X_GPGAuth_Logout_URL => '/index.pl?logout');
-	$gpg_headers->header(-X_GPGAuth_Pubkey_URL => '/localhost.pub');
+	$gpg_headers->header(X_GPGAuth_Version => '1.3.0');
+	$gpg_headers->header(X_GPGAuth_Requested => 'false');
+	$gpg_headers->header(X_GPGAuth_Verify_URL => '/index.pl?server_verify');
+	$gpg_headers->header(X_GPGAuth_Login_URL => '/index.pl?login');
+	$gpg_headers->header(X_GPGAuth_Logout_URL => '/index.pl?logout');
+	$gpg_headers->header(X_GPGAuth_Pubkey_URL => '/localhost.pub');
 
 	my $sid = $q->cookie('keyid') || $q->param('keyid') || undef;
 
@@ -63,8 +67,8 @@ while (my $q = new CGI::Fast) {
 
 	if (!$sid) {
 
-		$gpg_headers->header(-X_GPGAuth_Progress => 'stage0');
-		$gpg_headers->header(-X_GPGAuth_Authenticated => 'false');
+		$gpg_headers->header(X_GPGAuth_Progress => 'stage0');
+		$gpg_headers->header(X_GPGAuth_Authenticated => 'false');
 
 		my $request_gpgauth = 'false';
 		my @url_params = $q->url_param('keywords');
@@ -77,7 +81,7 @@ while (my $q = new CGI::Fast) {
 			}
 		}
 
-		$gpg_headers->header(-X_GPGAuth_Requested => $request_gpgauth);
+		$gpg_headers->header(X_GPGAuth_Requested => $request_gpgauth);
 
 		if ($q->param('gpg_auth:server_verify_token')) {
 			my $ciphertext = $q->param('gpg_auth:server_verify_token');
@@ -85,17 +89,35 @@ while (my $q = new CGI::Fast) {
 			eval {
 				my $plaintext = $pgp->decrypt(Data => $ciphertext, Passphrase => 'gpg');
 				# need to set server reponse to plaintext here too?
-				$gpg_headers->header(-X_GPGAuth_Verify_Response => $plaintext);
+				$gpg_headers->header(X_GPGAuth_Verify_Response => $plaintext);
 			};
 			if ($@) {
-				$gpg_headers->header(-X_GPGAuth_Error => 'true');
-				$gpg_headers->header(-X_GPGAuth_Verify_Response => $pgp->errstr);
+				$gpg_headers->header(X_GPGAuth_Error => 'true');
+				$gpg_headers->header(X_GPGAuth_Verify_Response => $pgp->errstr);
 			}
 
 		}
 		elsif ($q->param('gpg_auth:keyid')) {
 			if (!$q->param('gpg_auth:user_token_result')) {
-				$gpg_headers->header(-X_GPGAuth_Progress => 'stage1');
+				$gpg_headers->header(X_GPGAuth_Progress => 'stage1');
+				my $keyid = $q->param('gpg_auth:keyid');
+
+				# make sure this is supposed to be md5_hex and not md5_base64 or something
+				my $nonce = md5_hex(irand());
+
+				my $plaintext = 'gpgauthv1.3.0|' . length($nonce) . '|' . $nonce . '|gpgauthv1.3.0';
+
+				eval {
+					my $encrypted_data = $pgp->encrypt(Recipients => $keyid, Data => $plaintext);
+					my $ciphertext = $pgp->sign(Data => $encrypted_data, Armour => 1, KeyID => '4c643f9e268c4b86', Passphrase => 'gpg');
+					warn($pgp->errstr . '\n');
+					$gpg_headers->header(X_GPGAuth_User_Auth_Token => quotemeta(uri_escape($ciphertext)) );
+				};
+				if ($@) {
+					$gpg_headers->header(X_GPGAuth_Error => 'true');
+					$gpg_headers->header(X_GPGAuth_User_Auth_Token => $pgp->errstr);
+
+				}
 			}
 		}
 
