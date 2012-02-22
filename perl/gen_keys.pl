963213e0be6f549a70de48489a8f174929b64634
Kyle Milz <kmilz@ucalgary.ca>
2012-02-21 18:13:07 -0700

    perl: add a key generation script.
    Add a pure perl key generation script via OpenPGP.
    Don't have to use gnupg at all for the server side.

---
+++ perl/gen_keys.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use warnings;
+#use strict;
+
+require Crypt::OpenPGP;
+require Crypt::OpenPGP::Armour;
+
+
+($pub, $sec) = Crypt::OpenPGP->keygen(Type =>'RSA', Size => '2048', Identity => 'localhost', Passphrase => 'gpg');
+
+open FILE, ">localhost.sec" or die $!;
+print FILE $sec->save;
+close FILE;
+
+open FILE, ">localhost.pub" or die $!;
+print FILE Crypt::OpenPGP::Armour->armour(Data => $pub->save, Object => "PUBLIC KEY BLOCK");
+close FILE;
+
+
