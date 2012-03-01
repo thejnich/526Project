3feb75da7954f5a13991e02b15ae23e550930128
Masud Khan <masudio@gmail.com>
2012-03-01 13:46:01 -0700

    sending gpgauth headers, icon showing up.  headers need to be changed so they're valid.

--- ruby/app/controllers/home_controller.rb
+++ ruby/app/controllers/home_controller.rb
@@ -1,8 +1,24 @@
-#require 'net/http'
+require 'open-uri' # need this or else HTML(open()) will sometimes look in local directories instead of internet
 
 class HomeController < ApplicationController
 
   def home
+    response.headers["X-GPGAuth-Version"] = "1.3.0";
+    response.headers["X-GPGAuth-Requested"] = "false";
+    # The following headers describe to the client where to look for resources;
+    # The way this example PHP page actually operates, the '?page' are just for 
+    # differentiating the requests while debugging/testing - the only page that requires
+    # a "?page" querystring variable in this example is the logout page.
+    # All of these paths must be relative to the root of the domain. Nothing else will
+    # be permitted by the client.
+    response.headers["X-GPGAuth-Verify-URL"] = "/home/some_server_verify_URL";
+    response.headers["X-GPGAuth-Login-URL"] = "/home/some_login_url";
+    response.headers["X-GPGAuth-Logout-URL"] = "/home/some_logout_url";
+    # This points to an ACII armored public key (can be multiple keys in one file)
+    # that is used to allow the client to import the servers pubic key.
+    response.headers["X-GPGAuth-Pubkey-URL"] = ".gnupg/localhost.pub";
+    #header('X-GPGAuth-Pubkey-URL: /localhost.pub');
+
 #    `gpg --homedir /home/masudio/development/cpsc526/526Project/ruby/.gnupg --keyserver keyserver.ubuntu.com --search-keys makha@ucalgary.ca < 1`
 
     doc = Nokogiri::HTML(open('http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=makha@ucalgary.ca'))
