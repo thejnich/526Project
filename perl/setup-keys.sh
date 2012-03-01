07a66d28b3784c53b1b97a5ee61513a45d37999f
Kyle Milz <kmilz@ucalgary.ca>
2012-03-01 01:00:02 -0700

    sh: add some verbosity to setup-keys.

--- perl/setup-keys.sh
+++ perl/setup-keys.sh
@@ -3,6 +3,10 @@
 # helper script to generate some new keys and set everything up
 # run this script in the directory where you want to store your secrets
 
+printf "WARNING\n"
+printf "use your domain name for the email address of the keys.\n"
+printf "WARNING\n\n"
+
 rm -rf .gnupg/pubring.gpg* .gnupg/secring.gpg .gnupg/trustdb.gpg server.pub
 mkdir -p .gnupg
 chmod 700 .gnupg
