34a5a59103f2e83b1cdb23a4f5fd2967155dd48e
Jeff Nicholson <jeff.nicholson99@gmail.com>
2012-03-09 14:36:12 -0700

    python: begin porting to python, stage0
    Beginning implementation in python. Server sends headers, extension
    correctly recognizes them. Stage 0 complete.

--- python/index.py
+++ python/index.py
@@ -8,9 +8,20 @@ from flup.server.fcgi import WSGIServer
 #import GnuPGInterface
 
 def app(environ, start_response):
-	start_response('200 OK', [('Content-Type', 'text/html')])
+	status = '200 ok'
+	response_headers = [('Content-Type', 'text/html'), 
+	('X-GPGAuth-Version', '1.3.0'),
+	('X-GPGAuth-Requested', 'false'),
+	('X-GPGAuth-Verify_URL', '/index.py?server_verify'),
+	('X-GPGAuth-Login_URL', '/index.py?login'),
+	('X-GPGAuth-Logout_URL', '/index.py?logout'),
+	('X-GPGAuth-Pubkey_URL', '/server.pub'),
+	('X-GPGAuth-Progress', 'stage0'),
+	('X-GPGAuth-Authenticated', 'false')]
 
-	yield '<h1>FastCGI Environment</h1>'
+	start_response(status, response_headers)
+
+	yield '<h1>You are not logged in</h1>'
 	yield '<table>'
 	for k, v in sorted(environ.items()):
 		yield '<tr><th>%s</th><td>%s</td></tr>' % (escape(k), escape(v))
