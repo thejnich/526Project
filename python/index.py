1cc2fe744a5b4143fcd554f091c94fea09d7174c
Jeff Nicholson <jeff.nicholson99@gmail.com>
2012-03-20 00:59:33 -0600

    python: up to stage 1
    Successfully send headers to extension. Extension responds
    with server_verify. Now need to use gpg library to respond

--- python/index.py
+++ python/index.py
@@ -1,47 +1,108 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-from cgi import escape
-import sys, os, Cookie, time
+from cgi import  escape
+import sys, os, Cookie 
 from flup.server.fcgi import WSGIServer
+from urlparse import parse_qs
 
 #import GnuPGInterface
 
-def showEnviron(response_body, environ):
-	response_body.append('<table>')
-	response_body.extend(['<tr><th>%s</th><td>%s</td></tr>' % (key, value) for key, value in sorted(environ.items())])
-	response_body.append('</table>')
+sessid = 'PYSESSID'
+
+page = ''' 
+<html>
+<head><title>Python GPGWebAuth</title></head>
+<body>
+%s
+</body>
+</html>
+'''
 
 
-def app(environ, start_response):
-	response_body = ['<h1>You are not logged in</h1>']
-	response_headers = [('Content-Type', 'text/html')]
+def showEnviron(environ):
+	env = []
+	env.append('<table>')
+	env.extend(['<tr><th>%s</th><td>%s</td></tr>' % (key, value) for key, value in sorted(environ.items())])
+	env.append('</table>')
+	return env
 
-	if 'HTTP_COOKIE' in environ and 'keyid' in environ['HTTP_COOKIE']:
-		response_body.append('found keyid')
-	else:
-		response_body.append('no keyid')
-		cookie = Cookie.SimpleCookie()
-		cookie['keyid'] = '1234'
-		response_headers.extend([('Set-Cookie', cookie.output(header=''))])	
+def myapp(environ, start_response):
 
-	response_body = '\n'.join(response_body)
 	status = '200 ok'
-	response_headers.extend([
-	('Content-Length', str(len(response_body))),
+
+	# create dictionary for response headers
+	response_headers = dict( [('Content-Type', 'text/html'),
+	('Content-Length', '0'),
 	('X-GPGAuth-Version', '1.3.0'),
 	('X-GPGAuth-Requested', 'false'),
-	('X-GPGAuth-Verify_URL', '/index.py?server_verify'),
-	('X-GPGAuth-Login_URL', '/index.py?login'),
-	('X-GPGAuth-Logout_URL', '/index.py?logout'),
-	('X-GPGAuth-Pubkey_URL', '/server.pub'),
+	('X-GPGAuth-Verify-URL', '/index.py?server_verify'),
+	('X-GPGAuth-Login-URL', '/index.py?login'),
+	('X-GPGAuth-Logout-URL', '/index.py?logout'),
+	('X-GPGAuth-Pubkey-URL', '/server.pub'),
 	('X-GPGAuth-Progress', 'stage0'),
-	('X-GPGAuth-Authenticated', 'false')])
+	('X-GPGAuth-Authenticated', 'false')] )
 
+	# body of response is a list of strings
+	response_body = ['<h1>Hello, and welcome to movie phone</h1>']
+
+	# get dictionary of query strings
+	url_params = parse_qs(environ['QUERY_STRING'], True)
+	
+
+
+	# use cookie to manage session, not functional
+	if not ('HTTP_COOKIE' in environ and sessid in environ['HTTP_COOKIE']):
+		# new session
+		response_body.append('new session')
+
+		if ('login' in url_params) or ('protected_content' in url_params):
+			response_headers['X-GPGAuth-Requested'] = 'true'
+		
+		try:
+			request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+		except (ValueError):
+				request_body_size = 0
+
+		request_body = environ['wsgi.input'].read(request_body_size)
+		post = parse_qs(request_body)
+
+		print >> sys.stderr, 'post:\n%s' % post
+		
+		if 'gpg_auth:server_verify_token' in post:
+			# client has requested server to verify itself
+			cipherText = post['gpg_auth:server_verify_token']
+
+			print >> sys.stderr, 'verifying server...'	
+			# now decrypt and send response back to client
+	
+
+		#cookie = Cookie.SimpleCookie()
+		#cookie[sessid] = '1234'
+		#response_headers['Set-Cookie'] = cookie.output(header='')	
+	else:
+		# The user already has a registered session
+		response_body.append('found session idi, THIS SHOULD NOT HAPPEN YET')
+
+
+	response_body.extend(showEnviron(environ))
+	# create one string
+	response_body = '\n'.join(response_body)
+	
+	# insert into body tag of page
+	ret = page % response_body
+
+	# set content length
+	response_headers['Content-Length'] = str(len(ret))
+
+	# convert into a list of tuples, as expected by start_response
+	response_headers = [(k,v) for k,v in response_headers.items()]
+	
 	start_response(status, response_headers)
 
+	return [ret]
 
-	return [response_body]
 
-WSGIServer(app).run()
+
+WSGIServer(myapp).run()
 
